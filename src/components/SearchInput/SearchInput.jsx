872ab9e30e4dfe18b212b6fd6d2175a8f768d2c9
Oksana Ivantsiv <59508997+Olja2020@users.noreply.github.com>
2024-09-30 10:24:36 +0300

    add

--- src/components/SearchInput/SearchInput.jsx
+++
@@ -1,3 +0,0 @@
-export const SearchInput =({camper})=>{
-    return 
-}
--- src/redux/campers/selectors.js
+++
@@ -1,8 +0,0 @@
-
-// export const selectCampers = (state) => state.campers.items;
-// export const selectIsLoading = (state) => state.campers.isLoading;
-// export const selectError = (state) => state.campers.error;
-
-// export const selectActiveCamperId = (state) => state.campers.activeCamperId;
-// export const selectCamperById = (state, camperId) =>
-//   state.campers.items.find((camper) => camper.id === camperId);
